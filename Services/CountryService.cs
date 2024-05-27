c16b84cc412acec23dc85cb2a97e1c6d0464a958
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-05-27 16:52:04 +0300

    Implemented CountryService using exernal API

---
+++ Services/CountryService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WPF_Supermarket.Services
+{
+    public class CountryService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<List<string>> GetAllCountriesAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://countriesnow.space/api/v0.1/countries/capital");
+            response.EnsureSuccessStatusCode();
+
+            var countriesJson = await response.Content.ReadAsStringAsync();
+            var countryResponse = JsonConvert.DeserializeObject<CountryResponse>(countriesJson);
+
+            List<string> countryNames = new List<string>();
+            foreach (var country in countryResponse.Data)
+            {
+                countryNames.Add(country.Name);
+            }
+
+            countryNames.Sort();
+            return countryNames;
+        }
+    }
+
+    public class CountryResponse
+    {
+        public bool Error { get; set; }
+        public string Msg { get; set; }
+        public List<Country> Data { get; set; }
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+    }
+}
